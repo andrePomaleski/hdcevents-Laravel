1b89053861cfa2939d8a5fed86d24d4f490bd079
André Pomaleski <andrepomaleski@hotmail.com>
2024-05-28 23:08:11 -0300

    Adicionada a funcionalidade e migration de relação entre participantes e eventos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
             <div class="card-body">
                 <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
-                <div class="card-participants">X Participantes</div>
+                <p class="card-participants">{{ count($event->users) }} {{ count($event->users) <= 1 ? 'Participante' : 'Participantes' }}</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
